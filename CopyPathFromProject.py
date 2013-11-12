e8a4ada626dd82708be0878e3b118a7eb0235dcc
Kazuyuki Kohno <kkohno@twitter.com>
2013-11-12 15:56:55 +0900

    add command to copy with line number

--- CopyPathFromProject.py
+++ CopyPathFromProject.py
@@ -1,9 +1,12 @@
 import sublime, sublime_plugin
 
 class CopyPathFromProjectCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
+    def run(self, edit, lineNumber=False):
         path = self.view.file_name()
         for folder in self.view.window().folders():
             path = path.replace(folder + '/', '', 1)
+        if lineNumber:
+          (row, col) = self.view.rowcol(self.view.sel()[0].a)
+          path = "%s:%s" % (path, row)
         sublime.set_clipboard(path)
         sublime.status_message('copied: %s' % path)
