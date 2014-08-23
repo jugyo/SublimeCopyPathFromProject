0dbbf4d927264df11abf379517acbde24a23373a
Kazuyuki Kohno <kkohno@twitter.com>
2014-08-23 05:31:44 -0700

    row + 1

--- CopyPathFromProject.py
+++ CopyPathFromProject.py
@@ -7,6 +7,6 @@ class CopyPathFromProjectCommand(sublime_plugin.TextCommand):
             path = path.replace(folder + '/', '', 1)
         if lineNumber:
           (row, col) = self.view.rowcol(self.view.sel()[0].a)
-          path = "%s:%s" % (path, row)
+          path = "%s:%s" % (path, row + 1)
         sublime.set_clipboard(path)
         sublime.status_message('copied: %s' % path)
